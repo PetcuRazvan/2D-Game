708456cc5687145d30fb37e9f696e65f0692eb5a
unknown <petcurazvam012@gmail.com>
2026-02-06 01:32:46 +0200

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("nush ce fac");
+
+        GamePanel gamepanel = new GamePanel();
+        window.add(gamepanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamepanel.startgameThread();
+    }
+}
